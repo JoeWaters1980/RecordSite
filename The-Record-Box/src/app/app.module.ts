e7b48f28adf934929dbaacecc94d1f3c7cd90643
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2021-07-18 17:35:16 +0100

    adding routing

--- The-Record-Box/src/app/app.module.ts
+++ The-Record-Box/src/app/app.module.ts
@@ -9,6 +9,19 @@ import { HomeComponent } from './Components/home/home.component';
 import { VinylComponent } from './Components/vinyl/vinyl.component';
 import { AccessComponent } from './Components/access/access.component';
 import { LoginComponent } from './Components/login/login.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+
+/* Adding the routes for navigation */
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  { path: 'Login', component: LoginComponent },
+  { path: 'access', component: AccessComponent },
+  { path: 'vinyl', component: VinylComponent },
+
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +35,8 @@ import { LoginComponent } from './Components/login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes,{useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
