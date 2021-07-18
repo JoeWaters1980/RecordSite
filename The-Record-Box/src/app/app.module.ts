9e671aa85b070c1e66d1cccded7550baac4f189b
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2021-07-18 15:29:36 +0100

    added basic pages

--- The-Record-Box/src/app/app.module.ts
+++ The-Record-Box/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { NavComponent } from './Components/nav/nav.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Components/home/home.component';
+import { VinylComponent } from './Components/vinyl/vinyl.component';
+import { AccessComponent } from './Components/access/access.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    VinylComponent,
+    AccessComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
