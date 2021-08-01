13abc90f9339df202adeea214a6fb3e461eab56b
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2021-08-01 18:58:17 +0100

    bacis outline complete

--- The-Record-Box/src/app/app.module.ts
+++ The-Record-Box/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { NavComponent } from './Components/nav/nav.component';
@@ -10,7 +9,6 @@ import { VinylComponent } from './Components/vinyl/vinyl.component';
 import { AccessComponent } from './Components/access/access.component';
 import { LoginComponent } from './Components/login/login.component';
 import { Routes, RouterModule } from '@angular/router';
-
 
 
 /* Adding the routes for navigation */
