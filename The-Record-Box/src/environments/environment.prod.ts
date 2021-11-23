4ec5b1bba51061538bd16c7d0c1a2412305516f0
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2021-11-23 14:43:03 +0000

    updated lamda still not working

--- The-Record-Box/src/environments/environment.prod.ts
+++ The-Record-Box/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   dynamon:{
-    TableUrl:'https://cchymyb2gj.execute-api.eu-west-1.amazonaws.com/TestRecordStore/TheRecordBox'
+    TableUrl:'https://th4m6xiioa.execute-api.eu-west-1.amazonaws.com/Test/'
  }
 };
--- The-Record-Box/src/environments/environment.ts
+++ The-Record-Box/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  dynamoURL:'https://cchymyb2gj.execute-api.eu-west-1.amazonaws.com/Test/TheRecordBox'
+  dynamoURL:'https://th4m6xiioa.execute-api.eu-west-1.amazonaws.com/Test/'
   //  https://cchymyb2gj.execute-api.eu-west-1.amazonaws.com/Test/TheRecordBox-Cart
+  // https://th4m6xiioa.execute-api.eu-west-1.amazonaws.com/Test
 };
