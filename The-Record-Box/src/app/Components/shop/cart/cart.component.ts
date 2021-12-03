ba6db3026d966aba290ad5d46734d98993add4c6
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2021-12-03 10:46:45 +0000

    all running ok

--- The-Record-Box/src/app/Components/shop/cart/cart.component.ts
+++ The-Record-Box/src/app/Components/shop/cart/cart.component.ts
@@ -6,34 +6,28 @@ import { MessengerService } from 'src/services/messenger.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
   cartItems: Product[] = [];
 
-
-   cartTotal =0;
-  constructor(private msg:MessengerService) { }
+  cartTotal = 0;
+  constructor(private msg: MessengerService) {}
 
   ngOnInit() {
-this.msg.getMessage().subscribe(product =>{
-   console.log(product)
-   this.cartItems.push(product as Product);
-  this.cartItems.forEach(item =>{
-    this.cartTotal += item.price })
+
+   
+    this.msg.getMessage().subscribe((product) => {
+      console.log(product);
+      this.cartItems.push(product as Product);
+
+      this.cartItems.forEach((item) => {
+        this.cartTotal = item.price;
+      });
+    
+    });
+
   
-//  this.cartItems.push({
-//    product.name,
- })
-
-
-// this.cartTotal=0
-  // this.cartItems.forEach(item =>{
-  //   this.cartTotal += 
-  // })
-
-
-
   }
-
+  goToCheckOut(){}
 }
