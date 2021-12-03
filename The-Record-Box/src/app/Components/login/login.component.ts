0c77d5a630162502bdd2a9d732a999b770415694
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2021-12-03 17:04:58 +0000

    google login added with redicrect

--- The-Record-Box/src/app/Components/login/login.component.ts
+++ The-Record-Box/src/app/Components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GoogleLoginProvider } from 'angularx-social-login';
+import { SocialAuthService } from 'angularx-social-login';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   title='Login';
-  constructor() { }
+  constructor(private authService:SocialAuthService) { }
 
   ngOnInit(): void {
 
+  }
+
+  signInGoggle(){
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
   }
   SignUp(){
     
