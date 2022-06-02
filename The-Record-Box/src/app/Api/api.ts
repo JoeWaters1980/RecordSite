b5d39ea6223c0d71a7363a200eb3274610d5eae2
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2022-06-02 18:14:58 +0100

    updating lambda functions

--- The-Record-Box/src/app/Api/api.ts
+++ The-Record-Box/src/app/Api/api.ts
@@ -1,7 +1,10 @@
 import { environment } from "src/environments/environment";
 
 export const baseURl= environment.production ? 'https://th4m6xiioa.execute-api.eu-west-1.amazonaws.com/Test/': 'http//localhost:4200'
-export const dynamoURL ='https://th4m6xiioa.execute-api.eu-west-1.amazonaws.com/Test/';
+export const dynamoURL ='https://ahx9a7f274.execute-api.eu-west-1.amazonaws.com/beta/';
 export const cartUrl= 'https://th4m6xiioa.execute-api.eu-west-1.amazonaws.com/Test/cart';
-export const LoginUrl='https://the-record-box-login.auth.eu-west-1.amazoncognito.com/login?client_id=4bucb39n2tllqmr3stb9mvcc6o&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http://localhost:4200/account';
-export const logOutUrl='https://the-record-box-login.auth.eu-west-1.amazoncognito.com/logout?client_id=4bucb39n2tllqmr3stb9mvcc6o&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+profile&logout_uri=http://localhost:4200/Home';
+export const LoginUrl='https://recordboxstore.auth.eu-west-1.amazoncognito.com/login?client_id=tp8bd9a9po1s5769bl8t12t30&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http://localhost:4200/account';
+export const logOutUrl='https://recordboxstore.auth.eu-west-1.amazoncognito.com/logout?client_id=tp8bd9a9po1s5769bl8t12t30&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+profile&logout_uri=http://localhost:4200/Home';
+
+export const dynamoURL1 ='https://ahx9a7f274.execute-api.eu-west-1.amazonaws.com/beta/';
+export const cartUrl1= 'https://th4m6xiioa.execute-api.eu-west-1.amazonaws.com/Test/cart';
